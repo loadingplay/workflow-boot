790c96acb90451882cf1e77936ad9527e74cd892
EdderLp <roy@loadingplay.io>
2022-09-22 18:01:17 +0000

    fix/build bug in build

---
+++ tests/__main__.py
@@ -0,0 +1,9 @@
+import unittest
+from tests.app.test_get_list_not_found import GetDocumentsNotFoundTestCase
+
+
+__all__ = [
+    "GetDocumentsNotFoundTestCase"
+]
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
