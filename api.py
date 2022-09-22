ea32ca4369f5252ea25973882c8cebb6b3d0808c
EdderLp <roy@loadingplay.io>
2022-09-22 14:01:23 +0000

    fix/build bug in build

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 import logging
 from fastapi import FastAPI
 from fastapi import HTTPException
-
 from app.get_list_not_found import GetDocumentsNotFound
 from app.retry_document import PostDocumentsNotFound
 
