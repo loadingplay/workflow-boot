8062dc8bd38a01b290a618cf3b29c4ec7ce2d744
Manu <manuel@loadingplay.io>
2022-10-27 15:28:51 -0300

    fix exception on retry document

--- api.py
+++ api.py
@@ -19,7 +19,10 @@ def get_list_document_not_found(data: dict):
 @api.post("/v1/retry_document_failed")
 def post_retry_document(data: dict):
     logging.info(f"Arrived data retry document : {data}")
-    json_data = PostDocumentsNotFound(data)
-    data_json = json_data.get_documents()
-    logging.info(f"Output data retry document : {data_json}")
-    return data_json
+    try:
+        json_data = PostDocumentsNotFound(data)
+        data_json = json_data.get_documents()
+        logging.info(f"Output data retry document : {data_json}")
+        return data_json
+    except Exception as ex:
+        logging.info(f"Error on retry document: {ex}")
